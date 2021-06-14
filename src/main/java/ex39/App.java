966de1d8f4f2cf1ae2b4ab7d4f63bb4d4052074c
Melissa Cabrera <melisac105@gmail.com>
2021-06-14 00:37:09 -0500

    ex39

--- src/main/java/ex39/App.java
+++ src/main/java/ex39/App.java
@@ -1,4 +1,22 @@
+/*
+ UCF COP3330 Summer 2021 Assignment 2 Solution
+ Copyright 2021 Melissa Cabrera
+
+ Exercise 39 - Sorting Records
+ When you’re looking at results, you’ll want to be able to sort them so you can find what 
+ you’re looking for quickly or do some quick visual comparisons.
+
+ Given the following data set, create a program that sorts all employees by last name and 
+ prints them to the screen in a tabular format.
+
+ Constraint
+ - Implement the data using a list of maps
+ */
+
 package ex39;
 
 public class App {
+  
+
+
 }
---
+++ src/main/java/ex39/util/Output.java
@@ -0,0 +1,23 @@
+package ex39.util;
+
+import java.util.List;
+
+public class Output {
+
+    static public void printResults(List<Processing.Data> results) {
+        String leftAlignFormat = "| %-25s | %-20s | %-15s |%n";
+        String separator = "+---------------------------+----------------------+-----------------+";
+
+        System.out.println("Results: ");
+
+        System.out.println(separator);
+        System.out.format(leftAlignFormat, "Name", "Position", "Separation Date");
+        System.out.println(separator);
+
+        for (Processing.Data temp : results) {
+            System.out.format(leftAlignFormat, temp.name, temp.position, temp.separationDate);
+        }
+
+        System.out.println(separator);
+    }
+}
