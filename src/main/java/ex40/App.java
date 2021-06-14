475f37b66d94767e4398fd397e4d24fdaa1f1463
Melisac105 <70670919+Melisac105@users.noreply.github.com>
2021-06-13 22:19:07 -0500

    ex40 comment

--- src/main/java/ex40/App.java
+++ src/main/java/ex40/App.java
@@ -1,3 +1,20 @@
+/*
+ UCF COP3330 Summer 2021 Assignment 2 Solution
+ Copyright 2021 Melissa Cabrera
+
+ Exercise 40 - Filtering Records
+
+ Sorting records is helpful, but sometimes you need to filter down the results to find or display only what
+ you’re looking for.
+
+ Given the following data set create a program that lets a user locate all records that match the search string
+ by comparing the search string to the first or last name field.
+
+ Constraint
+ - Implement the data using an array of maps or an associative array.
+
+ */
+
 package ex40;
 
 import ex40.util.Output;
